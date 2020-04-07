3d68b697498d14b2f23658a278b6b83dc0689874
theabhishek07 <asharma0269@gmail.com>
2020-04-07 19:05:03 +0530

    EXP_2,3_Prog_5

---
+++ LAB_WORK/EXP2,3/class_object.java
@@ -0,0 +1,23 @@
+//Q5. Write a program using classes and object in java.
+
+//Program:-
+public class Student {
+     int student_age;
+public Student(String name) {
+      System.out.println("Nameis :" + name );
+   }
+   public void setAge( int age ) {
+      student_age = age;
+   }
+   public int getAge( ) {
+      System.out.println("Student's age is :" + student_age );
+      return student_age;
+   }
+   public static void main(String[] args) {
+      Student s = new Student( "EJ" );
+      s.setAge( 20 );
+      s.getAge( );
+      System.out.println("Variable Value :" + s.student_age );
+    }
+    }
+
