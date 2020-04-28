7feb748f489dd9e5aa47a936162e289fe3001816
theabhishek07 <asharma0269@gmail.com>
2020-04-29 02:54:12 +0530

    add.java

---
+++ LAB_WORK/EXP_11(Servlet)/First/add.java
@@ -0,0 +1,33 @@
+package practice_servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author GAURAV
+ */
+public class add extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            int a=Integer.parseInt(request.getParameter("no1"));
+                 int b=Integer.parseInt(request.getParameter("no2"));
+                 out.println("Addition is: "+(a+b));
+        }
+    }
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
