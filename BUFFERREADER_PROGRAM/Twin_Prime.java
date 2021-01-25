73a99153929a2e884efb740e39971bc05f61cc90
ABHISHEK SHARMA <asharma0269@gmail.com>
2021-01-25 23:51:11 +0530

    Update Twin_Prime.java

--- BUFFERREADER_PROGRAM/Twin_Prime.java
+++ BUFFERREADER_PROGRAM/Twin_Prime.java
@@ -1,21 +1,18 @@
 import java.io.*;
 class TWIN_PRIME 
 {
-	public static void main(String[] args) throws IOException 
-	{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a, b, c1 = 0, c2 = 0;
 		System.out.println("ENTER FIRST NUMBER");
 		a = Integer.parseInt(br.readLine());
 		System.out.println("ENTER SECOND NUMBER");
 		b = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= a; i++) 
-		{
+		for (int i = 1; i <= a; i++) {
 			if (a % i == 0)
 			c1++;
 		}
-		for (int j = 1; j <= b; j++) 
-		{
+		for (int j = 1; j <= b; j++) {
 			if (b % j == 0)
 			c2++;
 		}
