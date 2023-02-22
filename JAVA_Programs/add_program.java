bc65edf36647bb079a76724aac7964943d3fb68b
trintha <tri12@gmail.com>
2023-02-22 13:22:30 +0530

    changes done

--- JAVA_Programs/add_program.java
+++ JAVA_Programs/add_program.java
@@ -3,7 +3,7 @@ class add
     public static void main(String[] args)
  {
         int a = 10;
-        int b = 26;
+        int b = 24;
         int sum = a + b;
         System.out.println("The sum is: " + sum);
   }
